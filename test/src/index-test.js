494502cb26329711751826bae1d63ec7f41a51bd
vwkd <33468089+vwkd@users.noreply.github.com>
2025-05-07 22:07:30 +0200

    export members individually

--- test/src/index-test.js
+++ test/src/index-test.js
@@ -8,58 +8,58 @@
 
 import assert from 'assert';
 import {describe, it} from 'mocha';
-import tags from '../../lib/index.js';
+import {date, type, region, language, languages, search, subtags, check, tags} from '../../lib/index.js';
 
 describe('tags', function () {
 	it('date() returns file date', function() {
-		assert(/\d{4}\-\d{2}\-\d{2}/.test(tags.date()));
+		assert(/\d{4}\-\d{2}\-\d{2}/.test(date()));
 	});
 
 	it('type() returns subtag by type', function() {
 		var subtag;
 
-		subtag = tags.type('Latn', 'script');
+		subtag = type('Latn', 'script');
 		assert(subtag);
 		assert.equal(subtag.format(), 'Latn');
 		assert.equal(subtag.type(), 'script');
 
-		assert.equal(tags.type('en', 'script'), null);
+		assert.equal(type('en', 'script'), null);
 	});
 
 	it('region() returns subtag by region', function() {
 		var subtag;
 
-		subtag = tags.region('IQ');
+		subtag = region('IQ');
 		assert(subtag);
 		assert.equal(subtag.format(), 'IQ');
 		assert.equal(subtag.type(), 'region');
 
-		assert.equal(tags.region('en'), null);
+		assert.equal(region('en'), null);
 	});
 
     it('language() handles undefined argument', function() {
-        assert.ifError(tags.language(undefined));
+        assert.ifError(language(undefined));
     });
 
 	it('language() returns subtag by language', function() {
 		var subtag;
 
-		subtag = tags.language('en');
+		subtag = language('en');
 		assert(subtag);
 		assert.equal(subtag.format(), 'en');
 		assert.equal(subtag.type(), 'language');
 
-		assert.equal(tags.language('GB'), null);
+		assert.equal(language('GB'), null);
 	});
 
 	it('languages() returns all languages for macrolanguage', function() {
 		var subtags, err;
 
-		subtags = tags.languages('zh');
+		subtags = languages('zh');
 		assert(subtags.length > 0);
 
 		try {
-			assert.equal(tags.languages('en'));
+			assert.equal(languages('en'));
 		} catch (e) {
 			err = e;
 		}
@@ -71,7 +71,7 @@ describe('tags', function () {
 	it('search() matches descriptions', function() {
 		var subtags;
 
-		subtags = tags.search('Maltese');
+		subtags = search('Maltese');
 		assert(subtags.length > 0);
 
 		assert.equal(subtags[0].type(), 'language');
@@ -81,14 +81,14 @@ describe('tags', function () {
 		assert.equal(subtags[2].type(), 'extlang');
 		assert.equal(subtags[2].format(), 'mdl');
 
-		subtags = tags.search('Gibberish');
+		subtags = search('Gibberish');
 		assert.deepEqual(subtags, []);
 	});
 
 	it('search() puts exact match at the top', function() {
 		var subtags;
 
-		subtags = tags.search('Dari');
+		subtags = search('Dari');
 		assert(subtags.length > 0);
 
 		assert.equal(subtags[0].type(), 'language');
@@ -96,32 +96,32 @@ describe('tags', function () {
 	});
 
 	it('subtags() returns subtags', function() {
-		var subtags;
+		var subtags2;
 
-		subtags = tags.subtags('whatever');
-		assert.deepEqual(subtags, []);
+		subtags2 = subtags('whatever');
+		assert.deepEqual(subtags2, []);
 
-		subtags = tags.subtags('mt');
-		assert.equal(subtags.length, 2);
-		assert.equal(subtags[0].type(), 'language');
-		assert.equal(subtags[0].format(), 'mt');
-		assert.equal(subtags[1].type(), 'region');
-		assert.equal(subtags[1].format(), 'MT');
+		subtags2 = subtags('mt');
+		assert.equal(subtags2.length, 2);
+		assert.equal(subtags2[0].type(), 'language');
+		assert.equal(subtags2[0].format(), 'mt');
+		assert.equal(subtags2[1].type(), 'region');
+		assert.equal(subtags2[1].format(), 'MT');
 	});
 
 	it('subtags() is case insensitive', function() {
-		var subtags;
+		var subtags2;
 
 		// Test for issue #11.
-		subtags = tags.subtags('SgNw')
-		assert.equal(subtags.length, 1);
-		assert.equal(subtags[0].type(), 'script');
-		assert.equal(subtags[0].format(), 'Sgnw');
+		subtags2 = subtags('SgNw')
+		assert.equal(subtags2.length, 1);
+		assert.equal(subtags2[0].type(), 'script');
+		assert.equal(subtags2[0].format(), 'Sgnw');
 	});
 
 	it('check() checks tag validity', function() {
-		assert(tags.check('en'));
-		assert(!tags.check('mo'));
+		assert(check('en'));
+		assert(!check('mo'));
 	});
 
 	it('gets tag', function() {
